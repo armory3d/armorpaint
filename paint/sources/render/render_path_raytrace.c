86175fb7208b846a8fa163ba5e06302e495b5926
luboslenco <lubos.lenco@gmail.com>
2026-08-26 12:34:19 +0200

    paint: raytrace cache

--- paint/sources/render/render_path_raytrace.c
+++ paint/sources/render/render_path_raytrace.c
@@ -13,6 +13,9 @@ gpu_buffer_t *render_path_raytrace_ib;
 gpu_texture_t *render_path_raytrace_env_cdf      = NULL;
 static char   *render_path_raytrace_env_cdf_file = NULL;
 
+#define RENDER_PATH_RAYTRACE_MAX_FRAMES 8
+static i32 render_path_raytrace_frames_drawn = 0;
+
 static float render_path_raytrace_half_to_float(uint16_t h) {
 	uint32_t sign = (uint32_t)(h >> 15) << 31;
 	uint32_t exp  = (h >> 10) & 0x1f;
@@ -312,8 +315,11 @@ void render_path_raytrace_commands(bool use_live_layer) {
 		render_path_base_swap_buf("buf");
 	}
 
-	render_target_t *framebuffer = any_map_get(render_path_render_targets, "buf");
-	_gpu_raytrace_dispatch_rays(framebuffer->_image, render_path_raytrace_f32a);
+	if (render_path_raytrace_frames_drawn < RENDER_PATH_RAYTRACE_MAX_FRAMES) {
+		render_path_raytrace_frames_drawn++;
+		render_target_t *framebuffer = any_map_get(render_path_render_targets, "buf");
+		_gpu_raytrace_dispatch_rays(framebuffer->_image, render_path_raytrace_f32a);
+	}
 
 	g_context->ddirty--;
 	g_context->pdirty--;
@@ -384,7 +390,8 @@ void render_path_raytrace_draw(bool use_live_layer) {
 	bool is_live   = g_config->brush_live && render_path_paint_live_layer_drawn > 0;
 	bool is_player = g_config->workspace == WORKSPACE_PLAYER;
 	if (g_context->ddirty > 1 || g_context->pdirty > 0 || is_live || is_player) {
-		render_path_raytrace_frame = 0;
+		render_path_raytrace_frame        = 0;
+		render_path_raytrace_frames_drawn = 0;
 	}
 
 	render_path_raytrace_commands(use_live_layer);
